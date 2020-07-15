ae0fc6de4c2faa24f4cbbb069bc367f3c2296f63
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-15 20:17:08 +0700

    add data

--- populateBooksTableQuery.sql
+++ populateBooksTableQuery.sql
@@ -9,4 +9,6 @@ select 'dwef jbaw', AuthorId,'1234567688' from Authors where Name = 'Emilies Sto
 union all
 select 'Is this a book?', AuthorId,'1234567891888' from Authors where Name = 'Michael Collingwood'
 union all
-select 'How To Read a Book', AuthorId,'1234567891235' from Authors where Name = 'Hizkia Susanto';
+select 'How To Read a Book', AuthorId,'1234567891235' from Authors where Name = 'Hizkia Susanto'
+union all
+select 'Harry Poster Boy', AuthorId,'Swag' from Authors where Name = 'Ted While';
