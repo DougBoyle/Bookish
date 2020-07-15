d1646f0177a93de6970a22a4070780e850d9332e
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-15 18:36:17 +0700

    sql queries to create tables and populate the Authors and Books table

---
+++ populateBooksTableQuery.sql
@@ -0,0 +1,12 @@
+insert into Books(Title, AuthorId, ISBN)
+select 'How To Read a Book', AuthorId,'1234567891234' from Authors where Name = 'Douglas Boyle'
+union all
+select 'How To Book', AuthorId,'1234567891256' from Authors where Name = 'Hizkia Susanto'
+union all
+select 'This is a Book', AuthorId,'001' from Authors where Name = 'Douglas Boyle'
+union all
+select 'dwef jbaw', AuthorId,'1234567688' from Authors where Name = 'Emilies Stone'
+union all
+select 'Is this a book?', AuthorId,'1234567891888' from Authors where Name = 'Michael Collingwood'
+union all
+select 'How To Read a Book', AuthorId,'1234567891235' from Authors where Name = 'Hizkia Susanto';
